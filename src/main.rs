a0f6ed8c2bbc918c24b62a24b05bcb1014808d5b
Remy Wang <remywang@uw.edu>
2020-05-28 11:51:48 -0700

    Add asserts.

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,20 @@ pub fn gallop<T>(mut slice: &[T], mut cmp: impl FnMut(&T)->bool) -> &[T] {
 type Vertex = u32;
 type Edge = (Vertex, Vertex);
 
+fn sorted(s: &[u32]) -> bool {
+    let mut prev = u32::MIN;
+    for n in s {
+        if n < &prev {
+            return false;
+        } else {
+            prev = *n;
+        }
+    }
+    true
+}
+
 fn intersect(r: &[u32], s: &[u32]) -> Vec<u32> {
+    debug_assert!(sorted(r) && sorted(s));
     let mut r = r;
     let mut s = s;
     if r.len() > s.len() {
@@ -47,19 +60,25 @@ fn triangle_sort(mut r: &[(u32, Vec<u32>)], s: &[(u32, Vec<u32>)], mut t: &[(u32
 
     let r_x: Vec<_> = r.into_iter().map(|(x, _)| *x).collect();
     let t_x: Vec<_> = t.into_iter().map(|(x, _)| *x).collect();
+    let s_y: Vec<_> = s.into_iter().map(|(y, _)| *y).collect();
+
     let big_a = intersect(&r_x, &t_x);
     for a in big_a {
         r = gallop(r, |(x, _)| x < &a);
         let r_a = &r[0].1;
+        debug_assert_eq!(&r[0].0, &a);
+
+        let big_b = intersect(r_a, &s_y);
         t = gallop(t, |(x, _)| x < &a);
         let t_a = &t[0].1;
-        let s_y: Vec<_> = s.into_iter().map(|(y, _)| *y).collect();
-        let big_b = intersect(r_a, &s_y);
-        // NOTE this should reset s?
-        let mut s = s;
+        debug_assert_eq!(&t[0].0, &a);
+
+        // NOTE this should reset s
+        let mut s_ = s;
         for b in big_b {
-            s = gallop(s, |(y, _)| y < &b);
-            let s_b = &s[0].1;
+            s_ = gallop(s_, |(y, _)| y < &b);
+            let s_b = &s_[0].1;
+            debug_assert_eq!(&s_[0].0, &b);
             let big_c = intersect(s_b, t_a);
             for c in big_c {
                 result.push((a, b, c));
@@ -119,6 +138,7 @@ fn triangle_hash<'a>(r: &'a[Edge], s: &'a[Edge], t: &'a[Edge]) ->
     result
 }
 
+// NOTE should be sorted
 fn to_trie(r: &[(u32, u32)]) -> Vec<(u32, Vec<u32>)> {
     let mut result: Vec<(u32, Vec<u32>)> = vec![];
     for (x, y) in r {
