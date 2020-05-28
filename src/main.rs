ce137a0b88f384e8c2fe3a01c3b486798e80a173
Remy Wang <remywang@uw.edu>
2020-05-27 22:47:21 -0700

    Add sort-join.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,73 @@
 use std::collections::{HashMap, HashSet};
 
+// Taken from Frank McSherry's blog Worst-case optimal joins, in dataflow
+// advances slice to the first element not less than value
+pub fn gallop<T>(mut slice: &[T], mut cmp: impl FnMut(&T)->bool) -> &[T] {
+    // if empty slice, or already >= element, return
+    if slice.len() > 0 && cmp(&slice[0]) {
+        let mut step = 1;
+        while step < slice.len() && cmp(&slice[step]) {
+            slice = &slice[step..];
+            step = step << 1;
+        }
+
+        step = step >> 1;
+        while step > 0 {
+            if step < slice.len() && cmp(&slice[step]) {
+                slice = &slice[step..];
+            }
+            step = step >> 1;
+        }
+
+        slice = &slice[1..]; // advance one, as we always stayed < value
+    }
+
+    return slice;
+}
+
 type Vertex = u32;
 type Edge = (Vertex, Vertex);
 
+fn intersect(r: &[u32], s: &[u32]) -> Vec<u32> {
+    let mut r = r;
+    let mut s = s;
+    if r.len() > s.len() {
+        std::mem::swap(&mut r, &mut s);
+    }
+    r.into_iter().flat_map(|x| {
+        let t = gallop(s, |y| y < x );
+        if t[0] == *x { Some(*x) } else { None }
+    }).collect()
+}
+
+fn triangle_sort(r: &Vec<(u32, Vec<u32>)>, s: &Vec<(u32, Vec<u32>)>, t: &Vec<(u32, Vec<u32>)>) ->
+    Vec<(Vertex, Vertex, Vertex)>
+{
+    let mut result = vec![];
+
+    let r_x: Vec<_> = r.into_iter().map(|(x, _)| *x).collect();
+    let t_x: Vec<_> = t.into_iter().map(|(x, _)| *x).collect();
+    let big_a = intersect(&r_x, &t_x);
+    for a in big_a {
+        let r_a = &gallop(r, |(x, _)| x < &a)[0].1;
+        let t_a = &gallop(t, |(x, _)| x < &a)[0].1;
+        let s_y: Vec<_> = s.into_iter().map(|(y, _)| *y).collect();
+        let big_b = intersect(r_a, &s_y);
+        for b in big_b {
+            let s_b = &gallop(s, |(y, _)| y < &b)[0].1;
+            let big_c = intersect(s_b, t_a);
+            for c in big_c {
+                result.push((a, b, c));
+            }
+        }
+    }
+    result
+}
+
+
 // compute the triangle query Q(x,y,z) = R(x, y), S(y, z), T(z, x)
 // using generic join
-fn triangle<'a>(r: &'a[Edge], s: &'a[Edge], t: &'a[Edge]) ->
+fn triangle_hash<'a>(r: &'a[Edge], s: &'a[Edge], t: &'a[Edge]) ->
     Vec<(&'a Vertex, &'a Vertex, &'a Vertex)>
 {
     // hash r on x to be joined with t
@@ -52,7 +114,27 @@ fn triangle<'a>(r: &'a[Edge], s: &'a[Edge], t: &'a[Edge]) ->
     result
 }
 
+fn to_trie(r: &[(u32, u32)]) -> Vec<(u32, Vec<u32>)> {
+    let mut result: Vec<(u32, Vec<u32>)> = vec![];
+    for (x, y) in r {
+        if result.is_empty() || result.last().unwrap().0 != *x {
+            result.push((*x, vec![*y]));
+        } else {
+            result.last_mut().unwrap().1.push(*y);
+        }
+    }
+    result
+}
+
 fn main() {
-    let es = vec![];
-    triangle(&es, &es, &es);
+    let mut es = vec![];
+    triangle_hash(&es, &es, &es);
+    // Q(x,y,z) = R(x, y), S(y, z), T(z, x)
+    // variable order: x, y, z
+    es.sort_by(|(x_1, y_1), (x_2, y_2)| x_1.cmp(x_2).then(y_1.cmp(y_2)));
+    let r = &es;
+    let s = &es;
+    let mut t: Vec<(u32, u32)> = es.iter().map(|(x, y)| (*y, *x)).collect();
+    t.sort_by(|(x_1, y_1), (x_2, y_2)| x_1.cmp(x_2).then(y_1.cmp(y_2)));
+    triangle_sort(&to_trie(r), &to_trie(s), &to_trie(&t));
 }
