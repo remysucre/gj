c03fa16f9c1f59b6cf0fa8ec8256c3ec1fd74377
Remy Wang <remy.sucre@gmail.com>
2020-07-15 10:29:23 -0700

    tweak run script

--- bench_live_journal.sh
+++ bench_live_journal.sh
@@ -2,16 +2,7 @@ echo "START EXPERIMENT" >> time.log;
 echo `date` >> time.log; 
 for ((a=1; a <= 10; a++))
 do
-    echo $((1000000*a)) >> time.log; 
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
-    cargo run --release $((1000000*a)) < ../data/LiveJournal-rand.txt >> time.log;
+    echo $((100000*a)) >> time.log; 
+    cargo run --release $((100000*a)) < ../data/rand-LiveJournal.txt >> time.log;
 done
 echo "END EXPERIMENT" >> time.log
