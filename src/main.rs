7a91878e1e325990b4c6a351eda4d8ef7e4d7d1d
Remy Wang <remy.sucre@gmail.com>
2020-07-15 17:36:05 -0700

    Clean up and batch.

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,10 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let n = args[1].parse().unwrap();
 
-    community(n, 0.7, 0.1)
+    for sample in vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] {
+        for cross in vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0] {
+            println!("sample {} cross {}", sample, cross);
+            community(n, sample, cross)
+        }
+    }
 }
