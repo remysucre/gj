a1de274cc898919447b16196a10729befe04f2c6
Remy Wang <remy.sucre@gmail.com>
2020-07-20 15:27:46 -0700

    Rearrange.

--- src/lib.rs
+++ src/lib.rs
@@ -29,9 +29,48 @@ pub mod relation {
             Box<dyn Iterator<Item = (&'a Self::Key, &'a [Self::Res], &'a [O::Res])> + 'a>;
     }
 
+    // create indexes on the fly
+    pub fn triangle_otf<'a, IdxLeft, IdxUnit, R, F>(
+        r: &'a [(Value, Value)],
+        s: &'a [(Value, Value)],
+        t: &'a [(Value, Value)],
+        agg: F,
+    ) -> R
+    where
+        IdxLeft: Index<Key = Value, Res = Value>,
+        IdxUnit: Index<Key = Value, Res = ()>,
+        R: Default,
+        F: Fn(&mut R, (&Value, &Value, &Value)),
+    {
+        let mut result = R::default();
+        // create indexes on r, t, s
+        let r_x = IdxLeft::create(r.iter().copied());
+        let t_x = IdxLeft::create(t.iter().copied());
+        let s_y = IdxLeft::create(s.iter().copied());
+        for (a, ra, ta) in r_x.intersect(&t_x) {
+            // index residual relations r_a, t_a
+            let ra_y = IdxUnit::create(ra.iter().copied().map(|n| (n, ())));
+            let ta_z = IdxUnit::create(ta.iter().copied().map(|n| (n, ())));
+            for (b, sb, _rab) in s_y.intersect(&ra_y) {
+                // index residual relation s_b
+                let sb_z = IdxUnit::create(sb.iter().copied().map(|n| (n, ())));
+                for (c, _sbc, _tac) in sb_z.intersect(&ta_z) {
+                    agg(&mut result, (a, b, c));
+                }
+            }
+        }
+        result
+    }
+
+    // this is really a trie node
+    // K is the type of the key
+    // R is the type of each child, usually another trie or unit
     pub trait Trie<K, R> {
         fn create(r: impl Iterator<Item = (K, R)>) -> Self;
 
+        // TODO intersect should also handle more than 2 tries
+        // NOTE which cannot be done with the current structure
+        // unless I generate a bunch of intersects
         fn intersect<'a, S, T: Trie<K, S>>(&'a self, other: &'a T) ->
             Box<dyn Iterator<Item = (&'a K, &'a R, &'a S)> + 'a>;
 
@@ -78,39 +117,6 @@ pub mod relation {
         for (a, ra, ta) in r_x.intersect(&t_x) {
             for (b, sb, _rab) in s_y.intersect(ra) {
                 for (c, _sbc,_tac) in sb.intersect(ta) {
-                    agg(&mut result, (a, b, c));
-                }
-            }
-        }
-        result
-    }
-
-    // create indexes on the fly
-    pub fn triangle_otf<'a, IdxLeft, IdxUnit, R, F>(
-        r: &'a [(Value, Value)],
-        s: &'a [(Value, Value)],
-        t: &'a [(Value, Value)],
-        agg: F,
-    ) -> R
-    where
-        IdxLeft: Index<Key = Value, Res = Value>,
-        IdxUnit: Index<Key = Value, Res = ()>,
-        R: Default,
-        F: Fn(&mut R, (&Value, &Value, &Value)),
-    {
-        let mut result = R::default();
-        // create indexes on r, t, s
-        let r_x = IdxLeft::create(r.iter().copied());
-        let t_x = IdxLeft::create(t.iter().copied());
-        let s_y = IdxLeft::create(s.iter().copied());
-        for (a, ra, ta) in r_x.intersect(&t_x) {
-            // index residual relations r_a, t_a
-            let ra_y = IdxUnit::create(ra.iter().copied().map(|n| (n, ())));
-            let ta_z = IdxUnit::create(ta.iter().copied().map(|n| (n, ())));
-            for (b, sb, _rab) in s_y.intersect(&ra_y) {
-                // index residual relation s_b
-                let sb_z = IdxUnit::create(sb.iter().copied().map(|n| (n, ())));
-                for (c, _sbc, _tac) in sb_z.intersect(&ta_z) {
                     agg(&mut result, (a, b, c));
                 }
             }
