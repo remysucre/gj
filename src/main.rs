4716875e074d04cf03305b4d956d21c5d69a81ef
Remy Wang <remy.sucre@gmail.com>
2020-08-06 15:34:09 -0700

    Tweak.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use gj::experiments::*;
 // use gj::util::*;
 
 fn main() {
-    lj_sum(68993773);
+    //lj_sum(68993773);
+    community(100000, 0.7, 0.1);
 
     //let args: Vec<String> = std::env::args().collect();
     //let n: usize = args[1].parse().unwrap();
